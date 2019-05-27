0cdf7e65165a42037da058c78f878f44551bb61f
NhatDinh <nhat.ldinh@gmail.com>
2019-05-27 13:12:44 -0700

    resolve failed build

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import '../styles/header.css'
 
+import '../styles/header.css'
 class Header extends React.Component {
   constructor(props) {
     super(props)
